357a0d058d13c815297a5c98089095351ac9ac2a
Sampson <sampsonanaba4@gmail.com>
2026-02-25 21:55:23 -0500

    Scaffold Spring Boot backend with base architecture

---
+++ backend/src/test/java/com/splitter/backend/SplitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
